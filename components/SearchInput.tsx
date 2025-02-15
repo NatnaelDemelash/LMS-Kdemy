edb0a54758d78e7fd08efb9d113508cd1d1ed4de
NatnaelDemelash <natidemelash074@gmail.com>
2025-02-15 19:31:31 +0300

    change search input component fro client component to server component

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,32 +1,16 @@
-'use client';
-
 import { Search } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import React, { FormEvent, useState } from 'react';
+import Form from 'next/form';
 
 const SearchInput = () => {
-  const router = useRouter();
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (searchQuery.trim()) {
-      router.push(`/search/${encodeURIComponent(searchQuery.trim())}`);
-    }
-  };
-
   return (
-    <form className="relative w-full flex-1" onSubmit={handleSubmit}>
+    <Form action="/search" className="relative w-full flex-1">
       <input
         className="w-full px-3 py-2 pl-10 text-sm bg-background/80 focus:outline-none border-2 border-slate-600 rounded-full"
-        type="search"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        name="query"
         placeholder="Search for Courses"
       />
       <Search className="absolute h-4 w-4 top-1/2 left-3 -translate-y-1/2 text-muted-foreground" />
-    </form>
+    </Form>
   );
 };
 
