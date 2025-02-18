88fa1cfa7501d75d2e3c06daa0a283d1ef09eddd
NatnaelDemelash <natidemelash074@gmail.com>
2025-02-18 11:04:38 +0300

    create student type and add it to sanity content

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <div>PAGE</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "c8.alamy.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
