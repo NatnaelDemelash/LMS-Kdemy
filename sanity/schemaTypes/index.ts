6443e3740809c8c3b5cb9d1ffe46df811d883c3f
NatnaelDemelash <natidemelash074@gmail.com>
2025-02-19 15:33:31 +0300

    create different schema types

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,20 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { studentType } from "./studentType";
+import { courseType } from "./courseType";
+import { instructorType } from "./instructorType";
+import { categoryType } from "./categoryType";
+import { moduleType } from "./moduleType";
+import { lessonType } from "./lessonType";
+import { lessonCompletionType } from "./lessonCompletionType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [studentType],
+  types: [
+    studentType,
+    courseType,
+    instructorType,
+    categoryType,
+    moduleType,
+    lessonType,
+    lessonCompletionType,
+  ],
 };
